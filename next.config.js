78d05e7c7cee1f5d789c45867a0f5e6a888384ac
Francisco Sousa <francisco.sousa@hey.com>
2021-04-18 22:35:42 +0100

    Fixes

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
+if (!process.env.JWT_TOKEN_KEY) {
+  console.error(
+    "No JWT_TOKEN_KEY defined, please review your environment variables."
+  );
+
+  process.exit(-1);
+}
+
 const withRpc = require("next-rpc")({
   experimentalContext: true,
 });
